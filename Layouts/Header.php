ea8823b5e02a78f0bad4463fe5d614f3607348a9
hadymohamedmohamedmorsy <hady812012@gmai.com>
2022-03-06 11:54:48 +0200

    add

--- Layouts/Header.php
+++ Layouts/Header.php
@@ -1,35 +1,52 @@
 <nav>
-        <header class="container">
-            <div class="logo"> The Logo </div>
-            <div class="counter-linke">
-                <ul class="nav-links">
-                    <li> <a href="#"> Home </a></li>
-                    <li class="dropdown"> 
-                    <span class="icon-chevron-thin-down"></span>
-                        <a href="#" > About Us </a>
-                        <div class="conter-linke-dropdown" data-menu = ".First-dropdwon-menu">
-                            <!-- <ul class="First-dropdwon-menu">
-                                <li> <a href="#" > navigator </a></li>
-                            </ul> -->
-                        </div>
-                    </li>
-                    <li><a href="#"> Services</a>  </li>
-                    <li class="dropdown"> 
-                    <span class="icon-chevron-thin-down"></span>
-                        <a href="#"> Blogs</a>  
-                        <div class="conter-linke-dropdown" data-menu = ".secound-dropdwon-menu">
-                            <!-- <ul class="secound-dropdwon-menu">
-                                <li> <a href="#" > navigator </a></li>
-                            </ul> -->
-                        </div>
-                    </li>
-                    <li> <a href="#"> Contact Us</a> </li>
-                </ul>
+    <header class="container">
+        <div class="logo"> The Logo </div>
+        <div class="counter-linke">
+            <ul class="nav-links">
+                <li> <a href="#"> Home </a></li>
+                <li class="dropdown"> 
+                <span class="icon-chevron-thin-down"></span>
+                    <a href="#" > About Us </a>
+                    <div class="conter-linke-dropdown" data-menu = ".First-dropdwon-menu">
+                        <!-- <ul class="First-dropdwon-menu">
+                            <li> <a href="#" > navigator </a></li>
+                        </ul> -->
+                    </div>
+                </li>
+                <li><a href="#"> Services</a>  </li>
+                <li class="dropdown"> 
+                <span class="icon-chevron-thin-down"></span>
+                    <a href="#"> Blogs</a>  
+                    <div class="conter-linke-dropdown" data-menu = ".secound-dropdwon-menu">
+                        <!-- <ul class="secound-dropdwon-menu">
+                            <li> <a href="#" > navigator </a></li>
+                        </ul> -->
+                    </div>
+                </li>
+                <li> <a href="#"> Contact Us</a> </li>
+            </ul>
+        </div>
+        <div class="burger">
+            <span class="link-1"></span>
+            <span class="link-2"></span>
+            <span class="link-3"></span>
+        </div>
+    </header>
+</nav>
+<div class="header-cover">
+    <div class="image" data-src="" data-alt="">
+        <div class="content-image">
+            <div class="container">
+                <div class="row">
+                    <h3> Quality To Us Belongs </h3>
+                    <p> 
+                        Smal discription about smal discription about smal discription
+                        about smal discription about smal discription about smal
+                        discription about smal discription about smal discription about
+                        smal discription about smal discription about
+                    </p>
+                </div>
             </div>
-            <div class="burger">
-                <span class="link-1"></span>
-                <span class="link-2"></span>
-                <span class="link-3"></span>
-            </div>
-        </header>
-    </nav>
+        </div>
+    </div>
+</div>
---
+++ Layouts/index-Body.php
@@ -0,0 +1,126 @@
+<!-- Section About Safwa -->
+<div class="about-safwa">
+    <div class="container">
+        <div class="row">
+            <div class="display-grid">
+                <div class="header-text">
+                    <h2> AL-SAFWA </h2>
+                    <span>Engineering Industry</span>
+                </div>
+                <div class="text-content">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos hic at dolorem, 
+                    nobis quod dolorum molestias fuga tempora fugit natus possimus animi neque voluptas 
+                    consequatur exercitationem impedit pariatur distinctio libero.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Section About Safwa -->
+<!--  New Three Of Projects -->
+<div class="Projects-News">
+    <div class="container">
+        <div class="row">
+            <div class="grid-layout center">
+                <div class="lay-pro"> 
+                    <div class="image-circle">
+                        <img src="assists/images/pexels-christina-morillo-1181243.jpg" alt="">
+                    </div>
+                    <div class="content-pro">
+                        <h3> lorem Hids mows</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio error dolor amet debitis ipsa, temporibus similique dignissimos dolores! Libero tempore veritatis veniam, at sed minima quia obcaecati ad optio tenetur.</p>
+                    </div>
+                </div>
+                <div class="lay-pro"> 
+                    <div class="image-circle">
+                        <img src="assists/images/pexels-christina-morillo-1181243.jpg" alt="">
+                    </div>
+                    <div class="content-pro">
+                        <h3> lorem Hids mows</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio error dolor amet debitis ipsa, temporibus similique dignissimos dolores! Libero tempore veritatis veniam, at sed minima quia obcaecati ad optio tenetur.</p>
+                    </div>
+                </div>
+                <div class="lay-pro"> 
+                    <div class="image-circle">
+                        <img src="assists/images/pexels-christina-morillo-1181243.jpg" alt="">
+                    </div>
+                    <div class="content-pro">
+                        <h3> lorem Hids mows</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio error dolor amet debitis ipsa, temporibus similique dignissimos dolores! Libero tempore veritatis veniam, at sed minima quia obcaecati ad optio tenetur.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- New Three Of Projectsrojects -->
+<!-- Video -->
+<div class="video">
+    <div class="header-video">
+        <h2> Video </h2>
+        <video width="100%" height="100%" controls>
+            <source src="movie.mp4" type="video/mp4">
+            <source src="movie.ogg" type="video/ogg">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+
+</div>
+<!-- Video -->
+
+<!--information  -->
+<div class="information">
+    <div class="container">
+        <div class="row">
+            <div class="content-information">
+                <div class="grid-layout-2">
+                    <div class="content-info">
+                        <h2> lorem hitoz</h2>
+                        <p> 
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, cumque ipsum assumenda aliquam nemo ullam
+                            magnam officiis vero. Quaerat libero aut eveniet quidem necessitatibus
+                            vitae doloribus id mollitia beatae laboreLorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, cumque ipsum assumenda aliquam nemo ullam
+                            magnam officiis vero. Quaerat libero aut eveniet quidem necessitatibus
+                            vitae doloribus id mollitia beatae labore!
+
+                            vitae doloribus id mollitia beatae laboreLorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, cumque ipsum assumenda aliquam nemo ullam
+                            magnam officiis vero. Quaerat libero aut eveniet quidem necessitatibus
+                            vitae doloribus id mollitia beatae labore!
+
+                            vitae doloribus id mollitia beatae laboreLorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, cumque ipsum assumenda aliquam nemo ullam
+                            magnam officiis vero. Quaerat libero aut eveniet quidem necessitatibus
+                            vitae doloribus id mollitia beatae labore!
+                        </p>
+                    </div>
+                    <div class="content-info">
+                        <h2> lorem hitoz</h2>
+                        <p> 
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, cumque ipsum assumenda aliquam nemo ullam
+                            magnam officiis vero. Quaerat libero aut eveniet quidem necessitatibus
+                            vitae doloribus id mollitia beatae laboreLorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, cumque ipsum assumenda aliquam nemo ullam
+                            magnam officiis vero. Quaerat libero aut eveniet quidem necessitatibus
+                            vitae doloribus id mollitia beatae labore!
+
+                            vitae doloribus id mollitia beatae laboreLorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, cumque ipsum assumenda aliquam nemo ullam
+                            magnam officiis vero. Quaerat libero aut eveniet quidem necessitatibus
+                            vitae doloribus id mollitia beatae labore!
+
+                            vitae doloribus id mollitia beatae laboreLorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, cumque ipsum assumenda aliquam nemo ullam
+                            magnam officiis vero. Quaerat libero aut eveniet quidem necessitatibus
+                            vitae doloribus id mollitia beatae labore!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--information  -->
+
+<!-- Gallary -->
+
+<div class="Gallary">
+
+</div>
+
+<!-- Gallary -->
