c831bd8bf6d620c0177bb01f497bc5d5a490f5be
hadymohamedmohamedmorsy <hady812012@gmai.com>
2022-03-23 15:41:02 +0200

    add

--- Layouts/Header.php
+++ Layouts/Header.php
@@ -1,88 +1,134 @@
 
-<nav>
-    <header class="container">
-        <div class="counter-linke">
-            <ul class="nav-links">
-                <li>
-                    <a href="?Lang=EN"><img src="assists/images/britch.jpg" alt=""/> <span> EN </span> </a>
-                </li>
-                <li>
-                    <a href="?Lang=AR"> <img src="assists/images/egypt.png" alt=""/> <span> AR </span> </a>
-                </li>
-                <li>
-                    <a href="?Lang=FR"> <img src="assists/images/French.png" alt=""/> <span> FR </span> </a>
-                </li>
-            </ul>
+<header>
+    <!-- pop Menu  -->
+        <div class="menu">
+            <div class="container">
+                <div class="row">
+                    <ul>
+                        <li> 
+                            <a href="#">
+                                Home</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                About Us
+                            </a>
+                        </li>
+                        <li> 
+                            <a href="#">
+                                Services
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Products
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Mediahub
+                            </a>
+                            <ul>
+                                <li>  <a href="#"> Blogs</a> </li>
+                                <li>  <a href="#"> Events</a> </li>
+                                <li>  <a href="#"> News</a> </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="logo"> 
-            <img src="assists/images/LogoSafwa.png" alt=""> 
-        </div>
-        <div class="burger">
-            <span class="link-1"></span>
-            <span class="link-2"></span>
-            <span class="link-3"></span>
+    <!-- pop Menu  -->
+    <nav>
+        <div class="container header">
+            <div class="counter-linke">
+                <ul class="nav-links">
+                    <li>
+                        <a href="?Lang=EN"><img src="assists/images/britch.jpg" alt=""/> <span> EN </span> </a>
+                    </li>
+                    <li>
+                        <a href="?Lang=AR"> <img src="assists/images/egypt.png" alt=""/> <span> AR </span> </a>
+                    </li>
+                    <li>
+                        <a href="?Lang=FR"> <img src="assists/images/French.png" alt=""/> <span> FR </span> </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="logo"> 
+                <img src="assists/images/LogoSafwa.png" alt=""> 
+            </div>
+            <div class="burger">
+                <span class="link-1"></span>
+                <span class="link-2"></span>
+                <span class="link-3"></span>
+            </div>
         </div>
 
-    </header>
-    <a href="#" class="get-Quote">
-        <div class="content-shape">
-            <span> Get </span>  
-            <span> Quite </span>
-        </div>
-    </a>
-</nav>
-<div class="header-cover">
-    <div class="image" data-src="" data-alt="">
-        <div class="image-content"> 
-                <img src="assists/images/pexels-alexander-isreb-1797428.jpg" alt="">
-                <img src="assists/images/pexels-pixabay-236698.jpg" alt="">
-                <img src="assists/images/pexels-movidagrafica-barcelona-1474993.jpg" alt="">
-                <img src="assists/images/pexels-pixabay-247763.jpg" alt="">
-                <div class="content-image">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-6">
-                                <div class="content-image-content Translating">
-                                    <h3> Quality To Us Belongs </h3>
-                                    <p> 
-                                        Smal discription about smal discription about smal discription
-                                        about smal discription about smal discription about smal
-                                        discription about smal discription about smal discription about
-                                        smal discription about smal discription about
-                                    </p>
+        <a href="#" class="get-Quote">
+            <div class="content-shape">
+                <span> Get </span>  
+                <span> Quite </span>
+            </div>
+        </a>
+    </nav>
+    <div class="header-cover">
+        <div class="image" data-src="" data-alt="">
+            <div class="image-content"> 
+                    <img src="assists/images/pexels-alexander-isreb-1797428.jpg" alt="">
+                    <img src="assists/images/pexels-pixabay-236698.jpg" alt="">
+                    <img src="assists/images/pexels-movidagrafica-barcelona-1474993.jpg" alt="">
+                    <img src="assists/images/pexels-pixabay-247763.jpg" alt="">
+                    <div class="content-image">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="content-image-content Translating">
+                                        <h3> Quality To Us Belongs </h3>
+                                        <p> 
+                                            Smal discription about smal discription about smal discription
+                                            about smal discription about smal discription about smal
+                                            discription about smal discription about smal discription about
+                                            smal discription about smal discription about
+                                        </p>
+                                    </div>
+                                    <div class="content-image-content Translating">
+                                        <h3> Quality</h3>
+                                        <p> 
+                                            Smal discription about smal discription about smal discription
+                                            about smal discription about smal discription about smal
+                                            discription about smal discription about smal discription about
+                                            smal discription about smal discription about
+                                        </p>
+                                    </div>
+                                    <div class="content-image-content Translating">
+                                        <h3> Belongs </h3>
+                                        <p> 
+                                            Smal discription about smal discription about smal discription
+                                            about smal discription about smal discription about smal
+                                            discription about smal discription about smal discription about
+                                            smal discription about smal discription about
+                                        </p>
+                                    </div>
+                                    <div class="content-image-content Translating">
+                                        <h3> Fivith Local </h3>
+                                        <p> 
+                                            Smal discription about smal discription about smal discription
+                                            about smal discription about smal discription about smal
+                                            discription about smal discription about smal discription about
+                                            smal discription about smal discription about
+                                        </p>
+                                    </div>
                                 </div>
-                                <div class="content-image-content Translating">
-                                    <h3> Quality</h3>
-                                    <p> 
-                                        Smal discription about smal discription about smal discription
-                                        about smal discription about smal discription about smal
-                                        discription about smal discription about smal discription about
-                                        smal discription about smal discription about
-                                    </p>
-                                </div>
-                                <div class="content-image-content Translating">
-                                    <h3> Belongs </h3>
-                                    <p> 
-                                        Smal discription about smal discription about smal discription
-                                        about smal discription about smal discription about smal
-                                        discription about smal discription about smal discription about
-                                        smal discription about smal discription about
-                                    </p>
-                                </div>
-                                <div class="content-image-content Translating">
-                                    <h3> Fivith Local </h3>
-                                    <p> 
-                                        Smal discription about smal discription about smal discription
-                                        about smal discription about smal discription about smal
-                                        discription about smal discription about smal discription about
-                                        smal discription about smal discription about
-                                    </p>
-                                </div>
+                                <div class="col-6"></div>
                             </div>
-                            <div class="col-6"></div>
                         </div>
                     </div>
-                </div>
+            </div>
         </div>
     </div>
-</div>
+</header>
