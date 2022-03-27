76ca6e325a1150935b555fef70f91cd59c9dc846
hadymohamedmohamedmorsy <hady812012@gmai.com>
2022-03-27 12:01:22 +0200

    add

--- Layouts/index-Body.php
+++ Layouts/index-Body.php
@@ -1,1 +1,80 @@
+<!-- about Safwa  -->
+<section class="about-safwa margin-between-section">
+    <div class="container">
+        <div class="row center-flex">
+            <h1 class="company"> Al-Safwa </h1>
+            <p class="center"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium aut vitae debitis qui incidunt quasi quae repellat laudantium ducimus, natus quos. Debitis labore quam impedit ea mollitia, quas dolorum omnis.</p>
+        </div>
+        <div class="row section-parent-us">
+            <div class="col-5">
+                <h2> Dream Big </h2>
+                <h2 class="last-line"> Inspire The Word</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dignissimos fuga cupiditate doloremque nemo similique maiores eius eos sint labore, hic possimus nobis accusantium quaerat architecto numquam excepturi earum nesciunt.</p>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                </ul>
+            </div>
+            <div class="col-7">
+                <div class="section-right">
+                    <div class="row">
+                        <div class="col-7">
+                            <h2>Let's Grow <span> Together</span></h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis laudantium error consectetur adipisicing elit.</p>
+                            <a href="#"> 
+                                Call Us
+                                <span class="icon-arrow-right2"></span>
+                            </a>
+                        </div>
+                        <div class="col-5">
+                            <img src="assists/images/8104.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- about Safwa  -->
 
+<!-- number section -->
+<section class="number-server margin-between-section">
+    <div class="overlay"></div>
+    <div class="content-on-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="title-serve">
+                        <h2> We Serve To Meet</h2>
+                        <span> Your Industorial Needs </span>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="content-Details">
+                                <p> 25 </p>
+                                <span> Produts </span>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="content-Details">
+                                <p> 2000 </p>
+                                <span> Happy Clients </span>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="content-Details">
+                                <p> 9 </p>
+                                <span> Years Of Experince </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- number section -->
