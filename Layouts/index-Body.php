83596aa7f3a7597649fc54d9a45e2b85de6ff222
hadymohamedmohamedmorsy <hady812012@gmai.com>
2022-03-29 10:43:09 +0200

    add

--- Layouts/index-Body.php
+++ Layouts/index-Body.php
@@ -11,10 +11,10 @@
                 <h2 class="last-line"> Inspire The Word</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dignissimos fuga cupiditate doloremque nemo similique maiores eius eos sint labore, hic possimus nobis accusantium quaerat architecto numquam excepturi earum nesciunt.</p>
                 <ul>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur.</li>
                 </ul>
             </div>
             <div class="col-7">
@@ -78,3 +78,310 @@
     </div>
 </section>
 <!-- number section -->
+
+<!-- Clients -->
+<section class="clients margin-between-section">
+    <div class="container">
+        <div class="row">
+            <div class="title">
+                <h2> Clients </h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-1.png" alt="">
+                </div>
+            </div>
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-2.png" alt="">
+                </div>
+            </div>
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-3.png" alt="">
+                </div>
+            </div>
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-4.png" alt="">
+                </div>
+            </div>
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-1.png" alt="">
+                </div>
+            </div>
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-2.png" alt="">
+                </div>
+            </div>
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-3.png" alt="">
+                </div>
+            </div>
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-4.png" alt="">
+                </div>
+            </div>
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-1.png" alt="">
+                </div>
+            </div>
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-2.png" alt="">
+                </div>
+            </div>
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-3.png" alt="">
+                </div>
+            </div>
+            <div class="col-3 less-padding">
+                <div class="content-image">
+                    <img src="assists/images/client1-4.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Clients -->
+
+<!-- map -->
+<section class="map margin-between-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div>
+                    <h2 class="title"> Network </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quas perferendis, molestias non omnis mollitia reprehenderit.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores voluptatum a nesciunt numquam unde ut.
+                    </p>
+                </div>
+            </div>
+            <div class="col-6">
+                <img src="assists/images/network.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- map -->
+
+<!-- Contact Us  -->
+<section class="contact margin-between-section">
+
+    <div class="container">
+        <div class="row">
+            <h2 class="title"> Conatct Us </h2>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="contact-Company">
+                    <span class="icon-phone"></span>
+                    <div>
+                        <h4> Call Us </h4>
+                        <p class="links-contact"> <a href="#"> 01012315216 </a> , <a  href="#"> 01134216512 </a> </p>
+                    </div>
+                </div>
+                <div class="contact-Company">
+                    <span class="icon-mail4"></span>
+                    <div>
+                        <h4> Email Us  </h4>
+                        <p class="links-contact"> <a  href="#"> Hady812012@gmail.com </a> </p>
+                    </div>
+                </div>
+                <div class="contact-Company">
+                    <span class="icon-location2"></span>
+                    <div>
+                        <h4> Location  </h4>
+                        <p class="links-contact"> Egypt , Cairo city , shekh zaied </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <h2 class="title-secound-contact"> Welcome To Al-Safwa </h2>
+                <p class="text-respose"> Send Your Message And We will Response Before 24 Houres </p>
+                <form class="form">
+                    <div class="input-group">
+                        <input type="text" class="input" require id="name"/>
+                        <label for="name" class="label-name">
+                            <span class="content-name"> Name </span>
+                        </label>
+                    </div>
+                    <div class="input-group">
+                        <input type="number" class="input" require id="number"/>
+                        <label for="number" class="label-name">
+                            <span class="content-name"> Number </span>
+                        </label>
+                    </div>
+                    <div class="input-group">
+                        <input type="email" class="input" require id="email"/>
+                        <label for="email" class="label-name">
+                            <span class="content-name"> Email </span>
+                        </label>
+                    </div>
+                    <div class="input-group">
+                        <textarea class="input"> </textarea>
+                        <label for="Message" class="label-name">
+                            <span class="content-name"> Message </span>
+                        </label>
+                    </div>
+                    <button class="submit"> 
+                        sent Message 
+                        <span class="icon-arrow-right2"></span>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact Us  -->
+
+<!-- Testemonials -->
+<div class="testemonials margin-between-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="title"> What Our Clients Are Saying </h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <div class="details-testemonial">
+                    <span class="icon-stack"></span>
+                    <h2> 2989 + </h2>
+                    <p> Customer SatisFaction</p>
+                </div>
+            </div>
+            <div class="col-9 dynamic-perosnality">
+                <div class="row align-item-center">
+                    <div class="col-6">
+                        <div class="person">
+                            <div class="person-image">
+                                <img src="assists/images/IMG-Defult-Male.jpg" alt="">
+                            </div>
+                            <div class="person-details">
+                                <h3 class="name-person"> 
+                                    Hady Mohamed 
+                                    <span class="stars">
+                                        <span class="icon-stars"></span>
+                                        <span class="icon-stars"></span>
+                                        <span class="icon-stars"></span>
+                                        <span class="icon-stars"></span>
+                                    </span>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet consectetur</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 right">
+                        <span class="icon-quotes-right"></span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, nam? Maiores veritatis, 
+                            atque quas tempore exercitationem, delectus voluptates aperiam eum corporis beatae 
+                            accusamus unde quisquam eveniet pariatur debitis repellat laboriosam.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row center-flex">
+            <span class="active"></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+</div>
+<!-- Testemonials -->
+
+<!-- services -->
+<div class="services margin-between-section">
+    <div class="container">
+        <div class="row center-flex">
+            <h2 class="title"> Our Services </h2>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="content-serve">
+                    <img src="assists/images/light-bulb.png" alt="">
+                    <h3> Web Analysis </h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam tempora exercitationem perferendis non minima modi dolores, ducimus consequuntur ea temporibus. Quae assumenda dolor ipsa ducimus exercitationem! Error excepturi nemo pariatur.</p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="content-serve">
+                    <img src="assists/images/acquisition.png" alt="">
+                    <h3> Web Analysis </h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam tempora exercitationem perferendis non minima modi dolores, ducimus consequuntur ea temporibus. Quae assumenda dolor ipsa ducimus exercitationem! Error excepturi nemo pariatur.</p>
+
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="content-serve">
+                    <img src="assists/images/statistics.png" alt="">
+                    <h3> Web Analysis </h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam tempora exercitationem perferendis non minima modi dolores, ducimus consequuntur ea temporibus. Quae assumenda dolor ipsa ducimus exercitationem! Error excepturi nemo pariatur.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- services -->
+
+<!-- team -->
+<div class="team margin-between-section">
+    <div class="container">
+        <div class="row content-header-team align-item-center no-margin">
+            <div class="col-6">
+                <h2 class="title"> Our Teams </h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, officiis tenetur! Distinctio provident placeat omnis similique ea? Autem quibusdam sunt quidem totam! Error ex recusandae ea quae quia odit laborum?</p>
+            </div>
+            <div class="col-6">
+                <a class="link-team" href="#">
+                    Join Our Team  
+                    <span class="icon-arrow-right2"></span>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="image-team">
+                    <img src="assists/images/pexels-alexander-isreb-1797428.jpg" alt="">
+                    <div class="details-person">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="image-team">
+                    <img src="assists/images/pexels-alexander-isreb-1797428.jpg" alt="">
+                    <div class="details-person">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="image-team">
+                    <img src="assists/images/pexels-alexander-isreb-1797428.jpg" alt="">
+                    <div class="details-person">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- team -->
+
